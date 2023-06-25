4333dceb206ffaaac7d7ced466676a3f27036759
leonardovalle34 <leonardovalle18@gmail.com>
2023-06-24 22:53:52 -0300

    found the api for the countries

--- front/src/Pages/MainPage.tsx
+++ front/src/Pages/MainPage.tsx
@@ -2,11 +2,23 @@ import "./styles.css"
 import Modal from "../components/modal/Modal";
 import { useState } from "react"
 import { Search } from "monday-ui-react-core/icons";
+import mondaySdk from 'monday-sdk-js'
 import { useModalContext } from "../contextApi/ModalContext";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function MainPage(){
+    const sdk = mondaySdk()
     const [searchField , setSearchField] = useState("");
     const {isOpen , setIsOpen} = useModalContext()
+    
+    sdk.get("settings").then(res => {
+        // Add your code here to handle the response
+        console.log(res); // Example: Output the response to the console
+      }).catch(error => {
+        // Handle any errors that occurred during the API request
+        console.error("Error retrieving settings:", error);
+      });
 
     const handleSearchParam = (e:string)=>{
         setSearchField(e)
@@ -14,6 +26,15 @@ export default function MainPage(){
     const handleSubmit = ()=>{
         console.log(searchField)
     }
+
+    const test = async()=>{
+        const response = await axios.get('https://restcountries.com/v3.1/all');
+        console.log(response)
+    }
+
+    useEffect(()=>{
+        test()
+    },[])
 
     return (
         <div className="MainCard">
